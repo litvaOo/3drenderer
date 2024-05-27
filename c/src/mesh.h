e5aae1464302e68828cd6a8602bf43720b626978
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2024-05-27 05:45:10 +0200

    Render triangle vertices

---
+++ c/src/mesh.h
@@ -0,0 +1,11 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "triangle.h"
+#include "vector.h"
+#define N_MESH_VERTICES 8
+#define N_MESH_FACES 12
+
+extern vec3_t mesh_vertices[N_MESH_VERTICES];
+extern face_t mesh_faces[N_MESH_FACES];
+#endif
