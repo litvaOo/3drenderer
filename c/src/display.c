39f7f1d239937aaed115aefba6a21e716fcebab1
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2024-05-28 02:13:01 +0200

    Draw lines and triangles

--- c/src/display.c
+++ c/src/display.c
@@ -1,4 +1,6 @@
 #include "display.h"
+#include <math.h>
+#include <stdint.h>
 
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
@@ -58,6 +60,31 @@ void draw_grid(int multiple) {
         color_buffer[window_width * y + x] = 0xFFAAAAAA;
     }
   }
+}
+
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color) {
+  int delta_x = x1 - x0;
+  int delta_y = y1 - y0;
+  int longest_side_length = fmax(abs(delta_x), abs(delta_y));
+
+  float x_inc = delta_x / (float)longest_side_length;
+  float y_inc = delta_y / (float)longest_side_length;
+
+  float current_x = x0;
+  float current_y = y0;
+
+  for (int i = 0; i <= longest_side_length; i++) {
+    draw_pixel(round(current_x), round(current_y), color);
+    current_x += x_inc;
+    current_y += y_inc;
+  }
+}
+
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2,
+                   uint32_t color) {
+  draw_line(x0, y0, x1, y1, color);
+  draw_line(x1, y1, x2, y2, color);
+  draw_line(x2, y2, x0, y0, color);
 }
 
 void draw_rectangle(int x, int y, int width, int height, uint32_t color) {
--- c/src/main.c
+++ c/src/main.c
@@ -92,12 +92,9 @@ void render(void) {
 
   for (int i = 0; i < N_MESH_FACES; i++) {
     triangle_t triangle = triangles_to_render[i];
-    draw_rectangle(triangle.points[0].x, triangle.points[0].y, 3, 3,
-                   0xFFFFFF00);
-    draw_rectangle(triangle.points[1].x, triangle.points[1].y, 3, 3,
-                   0xFFFFFF00);
-    draw_rectangle(triangle.points[2].x, triangle.points[2].y, 3, 3,
-                   0xFFFFFF00);
+    draw_triangle(triangle.points[0].x, triangle.points[0].y,
+                  triangle.points[1].x, triangle.points[1].y,
+                  triangle.points[2].x, triangle.points[2].y, 0xFFFF00FF);
   }
 
   render_color_buffer();
